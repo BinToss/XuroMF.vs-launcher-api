3c6e33bab2a7bae5bc74145d547b357cf1c739bb
Noah Sherwin <noahrsherwin@gmail.com>
2026-06-08 16:58:31 -0700

    fix: change `DISCORD_DEV_MODE` to string template literals

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -7,7 +7,7 @@ declare global {
       DISCORD_BOT_TOKEN: string
       DISCORD_GUILD_ID: string
       DISCORD_APP_ID: string
-      DISCORD_DEV_MODE: boolean
+      DISCORD_DEV_MODE: `${boolean}` | Uppercase<`${boolean}`> | `${0 | 1}`
       DISCORD_DEV_ID: string
       DISCORD_PRIV_UPDATES_WEBHOOK: string
       DISCORD_PUBLIC_UPDATES_WEBHOOK: string
